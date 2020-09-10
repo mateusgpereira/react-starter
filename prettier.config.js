b8e8bba06845978772cb9c8bf5e723a709879c9e
Mateus Pereira <mateusgoncalvespereira@gmail.com>
2020-09-09 22:54:36 -0300

    fixing eslint settings issue and added new rule to prettier added styled components and global style to this starter

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   singleQuote: true,
+  jsxSingleQuote: true,
   semi: false,
-  trailingComma: 'none',
+  trailingComma: 'none'
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    outline: 0;
+    padding: 0;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 16px Roboto, sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`
